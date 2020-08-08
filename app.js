e7a28795a5b91dccb6a3572d0dc4c2f00da7df91
Gabe Byrne <byrnegr.gmail.com>
2020-08-08 19:43:39 -0400

    routes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI
+const users = require("./routes/api/users")
+const dogs = require("./routes/api/dogs")
 
 
 mongoose
@@ -10,6 +12,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Yay"));
+
+app.use("/api/users", users);
+app.use("/api/dogs", dogs);
+
+
 
 const port = process.env.PORT || 5000;
 
---
+++ routes/api/dogs.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+  res.json({
+    msg: "This is the dog route"
+  })
+})
+
+module.exports = router;
