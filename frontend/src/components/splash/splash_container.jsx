61aee719c9e5b3deee4a3169fbac330ff068655f
Jlin2235 <Jlin2235@gmail.com>
2020-08-10 13:44:59 -0400

    splash page initial setup

---
+++ frontend/src/components/splash/splash_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import SplashPage from './splash'
+
+const msp = dispatch => ({
+
+})
+
+const mdp = dispatch => ({
+
+})
+
+export default connect(msp,mdp)(SplashPage);
