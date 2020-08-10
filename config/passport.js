91b43448834e7ba26f10e080f06989ba2fbc6f0b
DanielGuardado <59269194+DanielGuardado@users.noreply.github.com>
2020-08-10 09:38:13 -0400

    Clean code

--- config/passport.js
+++ config/passport.js
@@ -12,28 +12,28 @@ options.secretOrKey = keys.secretOrKey;
 module.exports = (passport) => {
   passport.use(
     new JwtStrategy(options, (jwt_payload, done) => {
-      console.log(jwt_payload)
+      console.log(jwt_payload);
       // console.log(User)
       // console.log(User.shelter_name)
-       if (!jwt_payload.shelter_name)
-       {User.findById(jwt_payload.id)
-      .then((user) => {
-        if (user) {
-          return done(null, user);
-        }
-        return done(null, false);
-        
-      }).catch((err) => console.log(err));
+      if (!jwt_payload.shelter_name) {
+        User.findById(jwt_payload.id)
+          .then((user) => {
+            if (user) {
+              return done(null, user);
+            }
+            return done(null, false);
+          })
+          .catch((err) => console.log(err));
+      } else {
+        Shelter.findById(jwt_payload.id)
+          .then((shelter) => {
+            if (shelter) {
+              return done(null, shelter);
+            }
+            return done(null, false);
+          })
+          .catch((err) => console.log(err));
       }
-      else {
-      Shelter.findById(jwt_payload.id)
-        .then((shelter) => {
-          if (shelter) {
-            return done(null, shelter);
-          }
-          return done(null, false);
-        }).catch((err) => console.log(err));
-      
-    }})
+    })
   );
 };
