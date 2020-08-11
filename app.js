fcfd1ec963a70da3e013ecadbf2ca58479b7c98c
DanielGuardado <59269194+DanielGuardado@users.noreply.github.com>
2020-08-11 16:07:44 -0400

    fix heroku errors

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ const path = require("path");
 const users = require("./routes/api/users");
 const dogs = require("./routes/api/dogs");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
-
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -36,3 +29,9 @@ app.use("/api/dogs", dogs);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
