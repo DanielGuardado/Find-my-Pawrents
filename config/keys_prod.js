58ff1ced7e7b54a00a917a258123b7da0f43ccfb
DanielGuardado <59269194+DanielGuardado@users.noreply.github.com>
2020-08-11 15:34:53 -0400

    Heroku setup

---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dev:fdYASMQNff7UcMAa@cluster0.ynh32.mongodb.net/MERN-pawrents?retryWrites=true&w=majority",
+  secretOrKey: process.env.SECRET_OR_KEY,
+};
