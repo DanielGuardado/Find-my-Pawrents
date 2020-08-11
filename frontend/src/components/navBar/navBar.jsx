2931915eef8ac494693580712cea9d42db260f9a
Jlin2235 <Jlin2235@gmail.com>
2020-08-11 11:48:04 -0400

    resolve navBar conflict

--- frontend/src/components/navBar/navBar.jsx
+++ frontend/src/components/navBar/navBar.jsx
@@ -1,43 +1,70 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from './dogLogo2.png'
-import './navBar.scss'
+import logo from "./dogLogo2.png";
+import "./navBar.scss";
 
 // ({ currentUser, logout, openModal })
 
-class NavBar extends React.Component{
-    noUserNavBar() {
+class NavBar extends React.Component {
+  noUserNavBar() {
+    return (
+      <div>
+        <div className="noUserNavBar-container">
+          <div className="noUserNavBar-left-side-of-navbar">
+            <img id="logo" src={logo} />
+            <p>About</p>
+            <p>Contact</p>
+          </div>
+          <div className="noUserNavBar-right-side-of-navbar">
+            <button
+              className="noUserNavBar-nav-login-button"
+              onClick={() => this.props.openModal("login")}
+            >
+              Login
+            </button>
+            <button
+              className="noUserNavBar-nav-signup-button"
+              onClick={() => this.props.openModal("signup")}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-        return(
-            <div>
-                <div className='noUserNavBar-container'>
-                    <div className='noUserNavBar-left-side-of-navbar'>
-                        <img id='logo' src={logo}/>
-                        <p>About</p>
-                        <p>Contact</p>
-                    </div>
-                    <div className='noUserNavBar-right-side-of-navbar'>
-                        <button className="noUserNavBar-nav-login-button" onClick={() => this.props.openModal("login")}>
-                            Login
-                        </button>
-                        <button className="noUserNavBar-nav-signup-button" onClick={() => this.props.openModal("signup")}>
-                            Sign Up
-                        </button>
-                    </div>
-                </div>
-            </div>
-        )};
-
-   currentUserNavBar() {
-       debugger
-       return(
-            <div>
-               <div className='currentUserNavBar-container'>
-                    <h1> {this.props.currentUser} </h1>
-                   <button className="currentUserNavBar-nav-logout-button" onClick={()=> this.props.logout()}>logout</button>
-               </div>
-            </div>
-        )};
+  currentUserNavBar() {
+    if (this.props.currentUser.user.shelter_name) {
+      return (
+        <div>
+          <div className="currentUserNavBar-container">
+            <h1>{this.props.currentUser.user.shelter_name}</h1>
+            <button
+              className="currentUserNavBar-nav-logout-button"
+              onClick={() => this.props.logout()}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div className="currentUserNavBar-container">
+            <h1>Welcome back {this.props.currentUser.user.first_name}</h1>
+            <button
+              className="currentUserNavBar-nav-logout-button"
+              onClick={() => this.props.logout()}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      );
+    }
+  }
 
   // const shelterUserNavBar = () => (
   //   <div>
@@ -45,17 +72,15 @@ class NavBar extends React.Component{
   //   </div>
   // );
 
-        render(){
-
-            if (this.props.loggedIn) {
-              return this.currentUserNavBar();
-            // } else if (shelterUser.shelter.id) {
-            //   return shelterUserNavBar();
-            } else {
-              return this.noUserNavBar();
-            }
-          };
-
-        }
+  render() {
+    if (this.props.loggedIn) {
+      return this.currentUserNavBar();
+      // } else if (shelterUser.shelter.id) {
+      //   return shelterUserNavBar();
+    } else {
+      return this.noUserNavBar();
+    }
+  }
+}
 
 export default NavBar;
