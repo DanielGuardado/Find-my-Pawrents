d84fac64d245ca9362eb368fabfb7f06b2aa33d3
DanielGuardado <59269194+DanielGuardado@users.noreply.github.com>
2020-08-14 02:16:14 -0400

    testing cards

--- frontend/src/components/dogs/dog_index_item.jsx
+++ frontend/src/components/dogs/dog_index_item.jsx
@@ -24,21 +24,24 @@ class DogIndexItem extends React.Component {
     return (
       <ul>
         <div id="dog-index-page-card-container">
+          <li className="dog-item-name">
+            <span className="dog-age">
+              {dog.name} <span>Age: {dog.age}</span>
+            </span>
+          </li>
           <Link to={`/dogs/${dog._id}`}>
             <div id="dog-index-page-image-container">
-              <img id="dog-index-page-image" src={dog.image} alt="1" />
+              <img
+                className="dog-pad"
+                id="dog-index-page-image"
+                src={dog.image}
+                alt="1"
+              />
             </div>
-            <div className="dog-index-page-list-content-container">
-              <li>
-                <span>Name:</span> {dog.name}
-              </li>
-              <li>
-                <span>Age:</span>
-                {dog.age}
-              </li>
-              <li>
-                <span>Status:</span>
-                {dog.adoption_status}
+            <div className="dog-index-page-list-content-container list-pad">
+              <li className="stat-pad">
+                <span>Strenths: {dog.strengths}</span>
+                <span className="avail">Status: {dog.adoption_status}</span>
               </li>
               <div id="icon-container">
                 <img id="like-icon" src={likeicon} />
